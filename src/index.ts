824523351dc88280c93aafc329419f3f08e4dbd2
youngkwon02 <youngkwon02@naver.com>
2022-04-15 18:13:37 +0900

    feat: design Block class

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
-class Human {
-  name: string;
-  age: number;
-  gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
-
-const youngkwon = new Human("Youngkwon", 26, "Male")
-
-const sayHi = (person: Human) => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
-}
-
-console.log(sayHi(youngkwon));
 
 export {}
